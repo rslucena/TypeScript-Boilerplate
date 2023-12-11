8864b43189bf9bb1353e6afd56ec28b2a126bb63
Rodrigo Lucena <rodrigo@rslucena.com.br>
2023-12-11 19:37:30 -0300

    duplicate references

--- src/shared/schemas/users.ts
+++ src/shared/schemas/users.ts
@@ -1,8 +1,7 @@
-import { int, mysqlTable, text } from 'drizzle-orm/mysql-core'
+import { mysqlTable, text } from 'drizzle-orm/mysql-core'
 import { reffer } from '../repositories/repository'
 
 export const users = mysqlTable('users', {
-  id: int('id').primaryKey().autoincrement(),
   name: text('name'),
   ...reffer,
 })
