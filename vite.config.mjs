7beb32a91433634869edbb5ab8ebf3a4c703a891
Rodrigo Lucena <rodrigo@rslucena.com.br>
2023-12-20 20:41:51 -0300

    Implement Vitest and configure the model, aliases and global executions

---
+++ vite.config.mjs
@@ -0,0 +1,27 @@
+/// <reference types="vitest" />
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  test: {
+    globals: false,
+    environment: 'node',
+    exclude: ['node_modules', '.docker', '.git', '.vscode'],
+  },
+  resolve: {
+    alias: [
+      {
+        find: '@domain',
+        replacement: resolve(__dirname, './src/domain'),
+      },
+      {
+        find: '@shared',
+        replacement: resolve(__dirname, './src/shared'),
+      },
+      {
+        find: '@tests',
+        replacement: resolve(__dirname, './src/tests'),
+      },
+    ],
+  },
+})
