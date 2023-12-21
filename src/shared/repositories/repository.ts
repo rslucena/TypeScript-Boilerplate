1a7617225032a2488251cf1d5a3df1359439b0d1
Rodrigo Lucena <rodrigo@rslucena.com.br>
2023-12-20 21:58:29 -0300

    Remove console log in connection

--- src/shared/repositories/repository.ts
+++ src/shared/repositories/repository.ts
@@ -2,5 +2,4 @@ import { drizzle } from 'drizzle-orm/mysql2'
 import mysql from 'mysql2'
 import connection from './connection'
 
-console.log(connection)
 export default drizzle(mysql.createConnection(connection))
