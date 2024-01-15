3f7335ecd284d2069f1698e955a79620e16f2630
Rodrigo Lucena <rodrigo@rslucena.com.br>
2024-01-14 21:44:46 -0300

    remove logs related to requests, add casesentive

--- src/shared/server/bootstrap.ts
+++ src/shared/server/bootstrap.ts
@@ -2,14 +2,20 @@ import fastify, { FastifyInstance } from 'fastify'
 import { IncomingMessage, Server, ServerResponse } from 'http'
 
 export default async function (): Promise<FastifyInstance> {
+  
   const server: FastifyInstance<Server, IncomingMessage, ServerResponse> = fastify({
-    logger: { level: process.env.LOG_LEVEL },
+    caseSensitive: true,
     pluginTimeout: 10_000,
+    disableRequestLogging: true,
+    logger: { level: process.env.LOG_LEVEL },
   })
+
   server.get('/', (_request, reply) => reply.code(401).send())
+
   server.get<{ Querystring: { domain: string } }>('/ping/:domain', (_request, reply) => {
     reply.code(200).send({ pong: 'it worked!' })
   })
+
   server.listen({ port: Number(process.env.APP_PORT), host: '0.0.0.0' }, (err, address) => {
     if (err) {
       console.error(err)
@@ -17,5 +23,6 @@ export default async function (): Promise<FastifyInstance> {
     }
     console.debug(`Ready and listening for routes: \n  ${server.printRoutes()}`)
   })
+
   return server
 }
