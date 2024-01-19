bb9e738a51691707f85f1e1600e9c6f8c75241ea
Rodrigo Lucena <rodrigo@rslucena.com.br>
2024-01-19 12:44:22 -0300

    Update index for create webserver

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,18 @@
 import caches from '@shared/cache/actions'
 import { uuid } from '@shared/repositories/references'
+import webserver from '@shared/server/web'
+;(async () => {
+  //
+  await webserver()
 
-const uuid1 = uuid({ table: 'users', id: 1 })
-console.debug(uuid1)
+  const uuid1 = uuid({ table: 'users', id: 1 })
+  console.debug(uuid1)
 
-const uuid2 = await caches.json.set(uuid1, { test: 2 }, 10000)
-console.debug(uuid2)
+  const uuid2 = await caches.json.set(uuid1, { test: 2 }, 10000)
+  console.debug(uuid2)
 
-const cac = await caches.json.get(uuid1)
-console.debug(cac)
+  const cac = await caches.json.get(uuid1)
+  console.debug(cac)
 
-console.debug(await caches.text.get(uuid1))
+  console.debug(await caches.text.get(uuid1))
+})()
