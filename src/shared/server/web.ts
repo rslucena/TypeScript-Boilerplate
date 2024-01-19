3821493b94521339046613d22ca4c0c48c970128
Rodrigo Lucena <rodrigo@rslucena.com.br>
2024-01-19 18:24:08 -0300

    Link the log manager to the webserver

--- src/shared/server/web.ts
+++ src/shared/server/web.ts
@@ -1,14 +1,16 @@
 import fastifyCors from '@fastify/cors'
+import Logs, { loghandler } from '@shared/logs/handler'
 import cors from '@shared/settings/cors'
-import fastify, { FastifyInstance } from 'fastify'
-import { IncomingMessage, Server, ServerResponse } from 'http'
+import fastify from 'fastify'
 
-export default async function (): Promise<FastifyInstance> {
-  const web: FastifyInstance<Server, IncomingMessage, ServerResponse> = fastify({
+export default async function () {
+  //
+  const web = fastify({
+    logger: loghandler,
     caseSensitive: true,
     pluginTimeout: 20_000,
+    requestTimeout: 20_000,
     disableRequestLogging: true,
-    logger: { level: process.env.LOG_LEVEL },
   })
 
   web.register(fastifyCors, cors)
@@ -19,20 +21,14 @@ export default async function (): Promise<FastifyInstance> {
     reply.code(200).send({ pong: 'it worked!' })
   })
 
-  web.listen({ port: Number(process.env.APP_PORT), host: '0.0.0.0' }, (err, address) => {
-    if (err) {
-      console.error(err)
-      process.exit(1)
-    }
-    console.debug(`${web.printRoutes()}`)
+  web.listen({ port: Number(process.env.APP_PORT), host: '0.0.0.0' }, (err, anddress) => {
+    if (err) Logs.file.error(err.message, err, true)
+    Logs.console.info(`Server listening on ${anddress}`)
+    Logs.console.info(`${web.printRoutes()}`)
   })
 
   process.on('SIGTERM', () => process.exit())
   process.on('SIGINT', () => process.exit())
-  process.on('uncaughtException', (err) => web.log.error(err, 'Uncaught Exception thrown'))
-  process.on('unhandledRejection', (reasons, p) =>
-    web.log.error(reasons as any, 'Unhandled Rejection at Promise', p)
-  )
-
-  return web
+  process.on('uncaughtException', (err) => Logs.file.error('Uncaught Exception thrown', err))
+  process.on('unhandledRejection', (reasons) => Logs.file.error('Unhandled Rejection', reasons))
 }
