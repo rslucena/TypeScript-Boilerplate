1663f4f14d07b1e12571947b735d2cfa0c1395ed
Rodrigo Lucena <rodrigo@rslucena.com.br>
2024-02-01 20:36:58 -0300

    remove migrations, for current development only

--- src/shared/migrations/0000_dark_triton.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE `main`.`users` (
-	`id` int AUTO_INCREMENT NOT NULL,
-	`name` text,
-	`created_at` timestamp NOT NULL DEFAULT (now()),
-	`updated_at` timestamp ON UPDATE CURRENT_TIMESTAMP,
-	`delete_at` timestamp,
-	CONSTRAINT `users_id` PRIMARY KEY(`id`)
-);
