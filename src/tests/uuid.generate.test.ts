51f550a307e7b6e662c53e173e948f4b1c139c60
Rodrigo Lucena <rodrigo@rslucena.com.br>
2024-03-01 18:07:42 -0300

    Change the UUID generation test

--- src/tests/uuid.generate.test.ts
+++ src/tests/uuid.generate.test.ts
@@ -1,9 +1,9 @@
-import { uuid } from '@infrastructure/repositories/references'
+import { hash } from '@infrastructure/repositories/references'
 import { describe, expect, it } from 'vitest'
 
 describe('Test cache functions', () => {
-  it('It should be possible to create a UUID', async () => {
-    const uuid1 = uuid({ table: 'users', id: 1 })
-    expect(uuid1).toBeTypeOf('string')
+  it('It should be possible to create a hash', async () => {
+    const hash1 = hash({ table: 'users', id: 1 })
+    expect(hash1).toBeTypeOf('string')
   })
 })
