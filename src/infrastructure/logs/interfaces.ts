ceee99390ed9145fbceb2925025f121ee5ed0b21
Rodrigo Lucena <rodrigo@rslucena.com.br>
2024-03-01 18:07:10 -0300

    Change file name references

--- src/infrastructure/logs/interfaces.ts
+++ src/infrastructure/logs/interfaces.ts
@@ -1,9 +1,9 @@
-export default interface references {
-  console: actions
-  file: actions
+export default interface actions {
+  console: methods
+  file: methods
 }
 
-interface actions {
+interface methods {
   error: (message: string, props: Error | any, exit?: boolean) => void
   warn: (props: any, message?: string) => void
   info: (props: any) => void
