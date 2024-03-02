d41fde472a35b4cf59be8b67a6a8e01b2ad36d25
Rodrigo Lucena <rodrigo@rslucena.com.br>
2024-03-02 02:13:47 -0300

    Configures the Cross-origin Resource Sharing structure

--- src/infrastructure/settings/cors.ts
+++ src/infrastructure/settings/cors.ts
@@ -3,8 +3,10 @@ import { FastifyCorsOptions } from '@fastify/cors'
 export default <FastifyCorsOptions>{
   credentials: true,
   maxAge: 2 * 60 * 60,
-  optionsSuccessStatus: 200,
-  allowedHeaders: ['Authorization'],
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
   origin: process.env.APP_CORS_ORIGIN,
-  exposedHeaders: ['set-cookie', 'Authorization'],
+  exposedHeaders: ['set-cookie', 'authorization'],
+  allowedHeaders: ['content-type', 'authorization'],
+  methods: ['GET', 'PUT', 'POST', 'DELETE', 'OPTIONS'],
 }
