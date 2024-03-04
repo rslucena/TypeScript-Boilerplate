7cf31fecd5288aa67a7f8bca324f10ad0c41e96e
Rodrigo Lucena <rodrigo@rslucena.com.br>
2024-03-04 15:41:55 -0300

    Organizes the structure of cache functions

--- src/infrastructure/cache/actions.ts
+++ src/infrastructure/cache/actions.ts
@@ -1,27 +1,21 @@
 import client from './connection'
-import { actions } from './interfaces'
+import { actions, setmode } from './interfaces'
 
 const cache: actions = {
   text: {
     del: (hash) => client.del(hash),
     get: (hash) => client.get(hash).catch(() => null),
-    set: (hash, vals, ttl) => set('text', hash, vals, ttl).catch(() => ''),
+    set: (hash, vals, ttl) => set({ type: 'text', hash, vals, ttl }).catch(() => ''),
   },
   json: {
     del: (hash) => client.json.del(hash),
     get: (hash) => client.json.get(hash).catch(() => null),
-    set: (hash, vals, ttl, key) => set('json', hash, vals, ttl, key).catch(() => ''),
+    set: (hash, vals, ttl, key) => set({ type: 'json', hash, vals, ttl, key }).catch(() => ''),
   },
   ping: () => client.ping(),
 }
 
-async function set(
-  type: 'json' | 'text',
-  hash: string,
-  vals: any,
-  ttl?: number,
-  key?: string
-): Promise<string | null> {
+async function set({ type, hash, vals, ttl, key }: setmode): Promise<string | null> {
   const actions = {
     text: () => client.set(hash, vals),
     json: () => client.json.set(hash, key ?? '$', vals),
