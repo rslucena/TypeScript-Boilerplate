ee4724c4043d0ab491c07688381519b2b8444fbf
Rodrigo Lucena <rodrigo@rslucena.com.br>
2024-03-08 01:11:14 -0300

    Add page to repository structure

--- src/infrastructure/repositories/repository.ts
+++ src/infrastructure/repositories/repository.ts
@@ -1,5 +1,14 @@
+import Logs from '@infrastructure/logs/handler'
+import { DefaultLogger } from 'drizzle-orm'
+import { MySqlSelect } from 'drizzle-orm/mysql-core'
 import { drizzle } from 'drizzle-orm/mysql2'
 import mysql from 'mysql2'
 import connection from './connection'
 
-export default drizzle(mysql.createConnection(connection))
+export function queryPagination<T extends MySqlSelect>(qb: T, page: number, pageSize: number = 10) {
+  return qb.limit(pageSize).offset(page * pageSize)
+}
+
+export default drizzle(mysql.createConnection(connection), {
+  logger: new DefaultLogger({ writer: { write: (message) => Logs.file.info(message) } }),
+})
