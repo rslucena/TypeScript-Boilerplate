345dd9488ec5324311cc55749ff187386d8f00bd
Rodrigo Lucena <rodrigo@rslucena.com.br>
2024-03-13 02:01:01 -0300

    Restructure the repository for postgres

--- src/infrastructure/repositories/repository.ts
+++ src/infrastructure/repositories/repository.ts
@@ -1,14 +1,27 @@
 import Logs from '@infrastructure/logs/handler'
 import { DefaultLogger } from 'drizzle-orm'
-import { MySqlSelect } from 'drizzle-orm/mysql-core'
-import { drizzle } from 'drizzle-orm/mysql2'
-import mysql from 'mysql2'
+import { drizzle } from 'drizzle-orm/postgres-js'
+import postgres from 'postgres'
 import connection from './connection'
+import { PgSelect } from 'drizzle-orm/pg-core'
 
-export function queryPagination<T extends MySqlSelect>(qb: T, page: number, pageSize: number = 10) {
-  return qb.limit(pageSize).offset(page * pageSize)
+export function withPagination<T extends PgSelect>(qb: T, page: number, pageSize: number = 10) {
+  return qb.limit(1 * pageSize).offset(page * pageSize)
 }
 
-export default drizzle(mysql.createConnection(connection), {
+const manager = drizzle(postgres(connection), {
   logger: new DefaultLogger({ writer: { write: (message) => Logs.file.info(message) } }),
 })
+
+// const repository = {
+//   get: (table: PgTable) => manager.select().from(table),
+//   update: () => manager.update,
+//   insert: () => manager.insert,
+//   delete: () => manager.delete,
+//   page: withPagination,
+//   manager,
+// }
+
+// manager
+
+export default manager
