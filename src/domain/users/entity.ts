ccd0936ca42c4c3f2f5381eb579b7193dcc5b86f
Rodrigo Lucena <rodrigo@rslucena.com.br>
2024-03-15 01:47:38 -0300

    Fix default email size to fit as many as possible

--- src/domain/users/entity.ts
+++ src/domain/users/entity.ts
@@ -4,7 +4,7 @@ import { pgTable, varchar } from 'drizzle-orm/pg-core'
 const columns = {
   name: varchar('name', { length: 50 }).notNull(),
   lastName: varchar('lastName', { length: 100 }),
-  email: varchar('email', { length: 256 }).unique().notNull(),
+  email: varchar('email', { length: 400 }).unique().notNull(),
   password: varchar('password', { length: 100 }).notNull(),
 }
 
