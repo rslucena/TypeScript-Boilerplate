14b26f2ba7804c6071f6226c37607cf3f17f09a2
Rodrigo Lucena <rodrigo@rslucena.com.br>
2024-03-20 14:03:35 -0300

    Finalize Swagger settings

--- src/infrastructure/settings/swagger.ts
+++ src/infrastructure/settings/swagger.ts
@@ -3,11 +3,21 @@ import { FastifySwaggerUiOptions } from '@fastify/swagger-ui'
 import { jsonSchemaTransform } from 'fastify-type-provider-zod'
 
 export const SettingOptions: SwaggerOptions = {
-  openapi: {
+  swagger: {
     info: {
       title: String(process.env.APP_NAME),
       description: String(process.env.APP_DESCRIPTION),
       version: String(process.env.APP_VERSION),
+    },
+    schemes: ['http', 'https'],
+    consumes: ['application/json'],
+    produces: ['application/json'],
+    securityDefinitions: {
+      jwt: {
+        type: 'apiKey',
+        name: 'Authorization',
+        in: 'header',
+      },
     },
   },
   transform: jsonSchemaTransform,
@@ -16,7 +26,15 @@ export const SettingOptions: SwaggerOptions = {
 export const SettingOptionsUI: FastifySwaggerUiOptions = {
   routePrefix: '/documentation',
   theme: {
-    title: String(process.env.APP_TITLE),
+    title: String(process.env.APP_NAME),
+    js: [
+      {
+        filename: 'swaggertheme.js',
+        content:
+          '( new MutationObserver( () => document.querySelectorAll(".try-out__btn" ).forEach( ( el ) => el.remove() ) ).observe( document.body, { childList: true, subtree: true } ) );' +
+          '( new MutationObserver( () => document.querySelector( "#swagger-ui > section > div.topbar" ).remove()).observe( document.body, { childList: true, subtree: true } ) );',
+      },
+    ],
   },
   uiConfig: {
     docExpansion: 'list',
