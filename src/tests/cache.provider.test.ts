d90f919c8a6edabab474ba03b5c41f4ef7d2ff36
Rodrigo Lucena <rodrigo@rslucena.com.br>
2024-03-26 19:24:42 -0300

    updates the test framework to the cache

--- src/tests/cache.provider.test.ts
+++ src/tests/cache.provider.test.ts
@@ -8,50 +8,43 @@ describe('Test cache functions', () => {
   })
 
   it('Must be possible to insert content', async () => {
-    const ref = hash({ table: 1, id: 1 })
-    const action = await cache.text.set(ref, 'Single mode')
+    const action = await cache.text.set('test-text-key', 'Single mode')
     expect(action).toBe('OK')
   })
 
   it('Must be possible to insert json content', async () => {
     const ref = hash({ table: 1, id: 2 })
-    const action = await cache.json.set(ref, { test: 'test' })
+    const action = await cache.json.set('test-json-key', { test: 'test' })
     expect(action).toBe('OK')
   })
 
   it('Must be possible to get content', async () => {
-    const ref = hash({ table: 1, id: 1 })
-    const action = await cache.text.get(ref)
+    const action = await cache.text.get('test-text-key')
     expect(action).toEqual('Single mode')
   })
 
   it('Must be possible to get json content', async () => {
-    const ref = hash({ table: 1, id: 2 })
-    const action = await cache.json.get(ref)
+    const action = await cache.json.get('test-json-key')
     expect(action).toEqual({ test: 'test' })
   })
 
   it('Must be possible to update content', async () => {
-    const ref = hash({ table: 1, id: 1 })
-    const action = await cache.text.set(ref, 'Multi mode')
+    const action = await cache.text.set('test-text-key', 'Multi mode')
     expect(action).toBe('OK')
   })
 
   it('Must be possible to update json content', async () => {
-    const ref = hash({ table: 1, id: 2 })
-    const action = await cache.json.set(ref, { test: 'test2' })
+    const action = await cache.json.set('test-json-key', { test: 'test2' })
     expect(action).toBe('OK')
   })
 
   it('Must be possible to delete content', async () => {
-    const ref = hash({ table: 1, id: 1 })
-    const action = await cache.text.del(ref)
+    const action = await cache.text.del('test-text-key')
     expect(action).toBe(1)
   })
 
   it('Must be possible to delete json content', async () => {
-    const ref = hash({ table: 1, id: 2 })
-    const action = await cache.json.del(ref)
+    const action = await cache.json.del('test-json-key')
     expect(action).toBe(1)
   })
 })
