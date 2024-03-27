69a5d59b1ad5c979cae4b9c7f3409bf25708a2f9
Rodrigo Lucena <rodrigo@rslucena.com.br>
2024-03-27 02:54:38 -0300

    - Fixes validation - Adds all possible filters - Optimizes column retrieval - Adds pagination

--- src/domain/user/actions/GetFindByParams.ts
+++ src/domain/user/actions/GetFindByParams.ts
@@ -1,8 +1,9 @@
 import cache from '@infrastructure/cache/actions'
+import Logs from '@infrastructure/logs/handler'
 import { collection } from '@infrastructure/repositories/references'
 import repository, { withPagination } from '@infrastructure/repositories/repository'
 import { container } from '@infrastructure/server/request'
-import { desc, eq, ilike, or, sql } from 'drizzle-orm'
+import { and, desc, eq, getTableColumns, ilike, sql } from 'drizzle-orm'
 import user from '../entity'
 import { default as schema } from '../schema'
 
@@ -12,39 +13,38 @@ export default async function GetFindByParams(request: container) {
   const validRequest = await schema.actions.read.safeParseAsync(request.query())
   if (!validRequest.success) throw request.badRequest('get/user/{params}')
 
+  const { data } = validRequest
+
   const cached = await cache.json.get(collection('get/user/{params}', validRequest))
   if (cached) return cached
 
-  let { _page, _size, name, lastName, email, activated } = validRequest.data
+  if (data.name) data.name = `%${data.name}%`
+  if (data.lastName) data.lastName = `%${data.lastName}%`
+  if (data.email) data.email = `%${data.email}%`
+  if (data.createdAt) data.createdAt = `${data.createdAt}%`
+  if (data.deletedAt) data.deletedAt = `${data.deletedAt}%`
+  if (data.updatedAt) data.updatedAt = `${data.updatedAt}%`
 
-  if (name) name = `%${name}%`
-  if (lastName) lastName = `%${lastName}%`
-  if (email) email = `%${email}%`
-
+  const { password, ...outhers } = getTableColumns(user)
+  
   const prepare = repository
-    .select({
-      id: user.id,
-      name: user.name,
-      lastName: user.lastName,
-      email: user.email,
-      activated: user.activated,
-      createdAt: user.createdAt,
-      updatedAt: user.updatedAt,
-      deletedAt: user.deletedAt,
-    })
+    .select({ ...outhers })
     .from(user)
     .where(
-      or(
-        name ? ilike(user.name, sql.placeholder('name')) : undefined,
-        lastName ? ilike(user.lastName, sql.placeholder('lastName')) : undefined,
-        email ? ilike(user.email, sql.placeholder('email')) : undefined,
-        activated ? eq(user.activated, sql.placeholder('activated')) : undefined
+      and(
+        data.name ? ilike(user.name, sql.placeholder('name')) : undefined,
+        data.lastName ? ilike(user.lastName, sql.placeholder('lastName')) : undefined,
+        data.email ? ilike(user.email, sql.placeholder('email')) : undefined,
+        data.createdAt ? ilike(user.email, sql.placeholder('createdAt')) : undefined,
+        data.updatedAt ? ilike(user.email, sql.placeholder('updatedAt')) : undefined,
+        data.deletedAt ? ilike(user.email, sql.placeholder('deletedAt')) : undefined,
+        data.activated !== undefined ? eq(user.activated, sql.placeholder('activated')) : undefined
       )
     )
     .orderBy(desc(user.id))
     .$dynamic()
 
-  withPagination(prepare, _page, _size)
+  withPagination(prepare, data._page, data._pageSize)
 
   const content = await prepare.execute(validRequest.data)
 
