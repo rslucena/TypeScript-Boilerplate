afa02416fb5c43f744179a33ca84bf42a61609ee
Rodrigo Lucena <rodrigo@rslucena.com.br>
2024-03-28 02:11:40 -0300

    add typing to the Get response in the cache

--- src/infrastructure/cache/actions.ts
+++ src/infrastructure/cache/actions.ts
@@ -4,12 +4,12 @@ import { actions, setmode } from './interfaces'
 const cache: actions = {
   text: {
     del: (hash) => client.del(hash),
-    get: (hash) => client.get(hash).catch(() => null),
+    get: <t>(hash: string) => client.get(hash).catch(() => null) as t,
     set: (hash, vals, ttl) => set({ type: 'text', hash, vals, ttl }).catch(() => ''),
   },
   json: {
     del: (hash) => client.json.del(hash),
-    get: (hash) => client.json.get(hash).catch(() => null),
+    get: <t>(hash: string) => client.json.get(hash).catch(() => null) as t,
     set: (hash, vals, ttl, key) => set({ type: 'json', hash, vals, ttl, key }).catch(() => ''),
   },
   ping: () => client.ping(),
