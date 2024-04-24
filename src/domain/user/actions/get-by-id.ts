b6100604d7aa546c9e3ee39ccbb977750bb8c74f
Rodrigo Lucena <rodrigo@rslucena.com.br>
2024-04-24 19:57:39 -0300

    Solve problems with non-select columns

--- src/domain/user/actions/get-by-id.ts
+++ src/domain/user/actions/get-by-id.ts
@@ -2,7 +2,7 @@ import cache from '@infrastructure/cache/actions'
 import { tag } from '@infrastructure/repositories/references'
 import repository from '@infrastructure/repositories/repository'
 import { container } from '@infrastructure/server/request'
-import { desc, eq, sql } from 'drizzle-orm'
+import { desc, eq, getTableColumns, sql } from 'drizzle-orm'
 import user from '../entity'
 import { default as schema } from '../schema'
 
@@ -18,17 +18,10 @@ export default async function getById(request: container) {
   const cached = await cache.json.get<user[]>(reference)
   if (cached) return cached
 
+  const { password, ...outhers } = getTableColumns(user)
+
   const prepare = repository
-    .select({
-      id: user.id,
-      name: user.name,
-      lastName: user.lastName,
-      email: user.email,
-      activated: user.activated,
-      createdAt: user.createdAt,
-      updatedAt: user.updatedAt,
-      deletedAt: user.deletedAt,
-    })
+    .select(outhers)
     .from(user)
     .where(eq(user.id, sql.placeholder('id')))
     .limit(1)
