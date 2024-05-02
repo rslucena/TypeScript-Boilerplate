95f211d57b0cc18cb4d13dff1ab286a0d1e8d789
Rodrigo Lucena <rodrigo@rslucena.com.br>
2024-05-02 11:45:24 -0300

    convert to new eslint standard

---
+++ eslint.config.js
@@ -0,0 +1,28 @@
+export default [
+  {
+    files: ['*.{ts,tsx}'],
+    env: {
+      es2022: true,
+      node: true,
+    },
+    rules: {
+      'semi': 'error',
+      'no-unused-vars': 'warn',
+      'no-undef': 'warn',
+    },
+    languageOptions: {
+      globals: true,
+      ecmaVersion: 'latest',
+      parse: '@typescript-eslint/parser',
+    },
+    extends: [
+      'eslint:recommended',
+      'plugin:@typescript-eslint/eslint-recommended',
+      'plugin:@typescript-eslint/recommended',
+      'prettier',
+      'plugin:drizzle/recommended',
+    ],
+    plugins: ['drizzle', '@typescript-eslint', 'prettier'],
+    ignores: ['!node_modules/', '!.git/', '!.docker/', '!.vscode/', '!dist/', '!tests/'],
+  },
+]
