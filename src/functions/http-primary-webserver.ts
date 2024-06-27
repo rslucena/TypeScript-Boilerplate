1a50ea40885918cafdd9c3ad02b04ad36c1a13aa
Rodrigo Lucena <rodrigo@rslucena.com.br>
2024-06-27 00:42:04 -0300

    change the name of the main web server

---
+++ src/functions/http-primary-webserver.ts
@@ -0,0 +1,7 @@
+import userRoutes from '@domain/user/routes'
+import webserver from '@infrastructure/server/webserver'
+;(async () => {
+  const server = await webserver.create()
+  server.register(userRoutes, { prefix: '/api/v1/users' })
+  webserver.start(server, Number(process.env.PROCESS_PORT))
+})()
