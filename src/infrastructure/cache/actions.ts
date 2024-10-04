0fca59dd9fb2eede694877cbc567c475f8205f8b
Rodrigo Lucena <rodrigo@rslucena.com.br>
2024-10-04 01:08:30 -0300

    Refactor: Update `get` function to include scanning keys and processing multiple values
    - Modified the `get` function to use the new `scan` function, allowing retrieval of all keys matching a given hash pattern.
    - The function now handles multiple keys, retrieving and parsing their contents.
    - Improved flexibility for handling both JSON and text types with a per-key approach.
    - Introduced an object `{ [key: string]: t | null }` to store and return key-value pairs for multiple matching entries.
    - Ensures compatibility with Redis SCAN for large datasets, preventing potential blocking issues with larger keyspaces.
    
    Changes to be committed:
    	modified:   src/infrastructure/cache/actions.ts

--- src/infrastructure/cache/actions.ts
+++ src/infrastructure/cache/actions.ts
@@ -20,14 +20,19 @@ const cache: actions = {
 
 async function get<t>({ type, hash }: setmode, force: boolean = false): Promise<null | t> {
   if (force) return null
+  const keys = await scan(hash)
+  if (!keys) return null
   const actions = {
     text: async () => await client.get(hash),
     json: async () => await client.json.get(hash),
   }
-  const action = await actions[stack ? type : 'text']()
-  if (!action) return null
-  if (!stack) return safeParse<t>(action as string) ?? null
-  return action as t
+  const contents: { [key: string]: t | null } = {}
+  for (let i = 0; i < keys.length; i++) {
+    const action = await actions[stack ? type : 'text']()
+    if (!action) contents[hash] = null
+    if (!stack) contents[hash] = safeParse<t>(action as string) ?? (action as t)
+  }
+  return contents as t
 }
 
 async function set({ type, hash, vals, ttl, key }: setmode): Promise<string | null> {
@@ -42,10 +47,22 @@ async function set({ type, hash, vals, ttl, key }: setmode): Promise<string | nu
 }
 
 async function del({ hash }: setmode): Promise<number> {
-  const keys = await client.keys(`${hash}*`)
+  const keys = await scan(`${hash}*`)
   if (!keys.length) return 0
   for (const key of keys) await client.del(key)
   return keys.length
+}
+
+async function scan(hash: string) {
+  let cursor = 0
+  const keys = []
+  do {
+    const reply = await client.scan(cursor, { MATCH: hash })
+    cursor = reply.cursor
+    keys.push(...reply.keys)
+  } while (cursor !== 0)
+  if (!keys.length) return []
+  return keys
 }
 
 export default cache
