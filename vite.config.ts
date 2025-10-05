6dde0840273a256a8e80172ab00dda6eaf8fddde
Rodrigo Lucena <rodrigo@rslucena.com.br>
2025-10-04 23:50:05 -0300

    Tests: Adds the beginning of the implementation of tests

--- vite.config.ts
+++ vite.config.ts
@@ -1,38 +1,37 @@
-import { resolve } from 'path'
-import { defineConfig, loadEnv } from 'vite'
+import { resolve } from "node:path";
+import { defineConfig, loadEnv } from "vite";
+import { configDefaults } from "vitest/config";
 
-export default ({ mode }) => {
-  process.env = { ...process.env, ...loadEnv(mode, process.cwd(), 'VITE_') }
-  Object.entries(process.env).forEach(([key, value]) => {
-    const reff = key.replace(/^VITE_/, '')
-    if (!value) delete process.env[key]
-    if (!key.startsWith('VITE_')) delete process.env[key]
-    process.env[reff] = value
-  })
-  return defineConfig({
-    test: {
-      globals: false,
-      environment: 'node',
-      exclude: ['node_modules/**', '.docker/**', '.git/**', '.github/**', '.vscode/**', 'dist/**'],
-    },
-    define: {
-      'process.env': process.env,
-    },
-    resolve: {
-      alias: [
-        {
-          find: '@domain',
-          replacement: resolve(__dirname, './src/domain'),
-        },
-        {
-          find: '@infrastructure',
-          replacement: resolve(__dirname, './src/infrastructure'),
-        },
-        {
-          find: '@tests',
-          replacement: resolve(__dirname, './src/tests'),
-        },
-      ],
-    },
-  })
-}
+export default ({ mode }: { mode: string }) => {
+	process.env = { ...process.env, ...loadEnv(mode, process.cwd(), "VITE_") };
+	for (const [key, value] of Object.entries(process.env)) {
+		const reff = key.replace(/^VITE_/, "");
+		if (!value || !key.startsWith("VITE_")) {
+			delete process.env[key];
+		} else {
+			process.env[reff] = value;
+		}
+	}
+	return defineConfig({
+		test: {
+			globals: false,
+			environment: "node",
+			exclude: [...configDefaults.exclude, ".docker/**"],
+		},
+		define: {
+			"process.env": process.env,
+		},
+		resolve: {
+			alias: [
+				{
+					find: "@domain",
+					replacement: resolve(__dirname, "./src/domain"),
+				},
+				{
+					find: "@infrastructure",
+					replacement: resolve(__dirname, "./src/infrastructure"),
+				},
+			],
+		},
+	});
+};
