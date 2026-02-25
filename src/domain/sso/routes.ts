d18054b70f1704dcbbc10f0552a5e2aceed2c862
lucena <rodrigo@rslucena.com.br>
2026-02-25 18:47:09 -0300

    feat(sso): expose local login route

--- src/domain/sso/routes.ts
+++ src/domain/sso/routes.ts
@@ -2,6 +2,7 @@ import request from "@infrastructure/server/request";
 import type { FastifyInstance } from "fastify";
 import getAuthorize from "./actions/get-authorize";
 import getCallback from "./actions/get-callback";
+import postLocalLogin from "./actions/post-local-login";
 import schema from "./schema";
 
 export default async function ssoRoutes(api: FastifyInstance) {
@@ -33,5 +34,21 @@ export default async function ssoRoutes(api: FastifyInstance) {
 			},
 		},
 		request.noRestricted(getCallback),
+	);
+	api.post(
+		"/local",
+		{
+			schema: {
+				tags: ["SSO"],
+				description: "Handles local login with email and password.",
+				body: schema.actions.local,
+				headers: schema.actions.headers.omit({ authorization: true }),
+				response: {
+					200: schema.responses.profile,
+					...request.reply.schemas,
+				},
+			},
+		},
+		request.noRestricted(postLocalLogin),
 	);
 }
